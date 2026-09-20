695b318f60d419c7df80963e56145253af548a84
ArkanUbaidillah <arkanwarman@gmail.com>
2026-09-20 16:35:56 +0700

    Lengkapi project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-// Titik masuk bawaan supaya `flutter run` langsung jalan (menjalankan Praktikum 1).
-// File praktikum lain dijalankan dengan:
-//   flutter run -t lib/praktikum2/main1.dart
 import 'praktikum1/main.dart' as praktikum;
 
 void main() => praktikum.main();
